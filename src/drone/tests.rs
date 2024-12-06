77820a7dcbdc7706510151b1c1e99c013e352bf7
Plopmenz <plopmenz@gmail.com>
2024-12-06 13:06:05 +0100

    test_drone_new missing assertions

--- src/drone/tests.rs
+++ src/drone/tests.rs
@@ -2,6 +2,7 @@
 
 use crate::drone::MyDrone;
 use crossbeam_channel::{unbounded, Sender};
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use wg_2024::controller::{DroneCommand, DroneEvent};
 use wg_2024::drone::Drone;
@@ -20,10 +21,16 @@ fn test_drone_new() -> () {
     let drone = MyDrone::new(
         id,
         event_send.clone(),
-        command_recv,
-        packet_recv,
-        neighbors_send,
+        command_recv.clone(),
+        packet_recv.clone(),
+        neighbors_send.clone(),
         pdr,
     );
+
     assert_eq!(drone.id, id);
+    assert!(drone.controller_send.same_channel(&event_send));
+    assert!(drone.controller_recv.same_channel(&command_recv));
+    assert!(drone.packet_recv.same_channel(&packet_recv));
+    assert_eq!(drone.packet_send.len(), neighbors_send.len());
+    assert_eq!(drone.pdr, pdr);
 }
