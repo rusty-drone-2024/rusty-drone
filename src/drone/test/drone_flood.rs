aa06342d8c1220fc211e8e3313ccb0654487e067
Matteo Di Noia <matteodinoia02@gmail.com>
2024-12-29 11:11:51 +0100

    allow leaf drone to immediately return a response

--- src/drone/test/drone_flood.rs
+++ src/drone/test/drone_flood.rs
@@ -1,4 +1,5 @@
 #![cfg(test)]
+
 use crate::drone::test::{simple_drone_with_exit, simple_drone_with_two_exit};
 use rusty_tester::utils::data::{new_flood_request, new_flood_request_with_path};
 use wg_2024::controller::DroneEvent;
@@ -33,7 +34,8 @@ fn test_drone_flood_res_inclusive() {
         },
     );
 
-    let (options, mut drone, packet_exit) = simple_drone_with_exit(11, 1.0, 10);
+    // Require extra node to be sure that it is not a leaf
+    let (options, mut drone, packet_exit, _) = simple_drone_with_two_exit(11, 1.0, 10, 13);
     drone.handle_packet(&packet, false);
     drone.handle_packet(&packet, false);
 
@@ -70,8 +72,29 @@ fn test_drone_flood_res() {
         },
     );
 
-    let (options, mut drone, packet_exit) = simple_drone_with_exit(11, 1.0, 10);
+    let (options, mut drone, packet_exit, _) = simple_drone_with_two_exit(11, 1.0, 10, 13);
     drone.handle_packet(&packet, false);
+    drone.handle_packet(&packet, false);
+
+    assert_eq!(expected.clone(), packet_exit.try_recv().unwrap());
+
+    options.assert_expect_drone_event(DroneEvent::PacketSent(expected));
+    options.assert_expect_drone_event_fail();
+}
+
+#[test]
+fn test_drone_flood_res_leaf() {
+    let packet = new_flood_request(5, 7, 10, false);
+    let expected = Packet::new_flood_response(
+        SourceRoutingHeader::new(vec![11, 10], 1),
+        5,
+        FloodResponse {
+            flood_id: 7,
+            path_trace: vec![(11, NodeType::Drone)],
+        },
+    );
+
+    let (options, mut drone, packet_exit) = simple_drone_with_exit(11, 1.0, 10);
     drone.handle_packet(&packet, false);
 
     assert_eq!(expected.clone(), packet_exit.try_recv().unwrap());
